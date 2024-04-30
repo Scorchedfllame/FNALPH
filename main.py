857ae27ec7affcbf8f69d379751acc53b3b96bf7
Scorchedfllame <85722296+Scorchedfllame@users.noreply.github.com>
2024-04-30 14:58:20 -0600

    Refactor fading loading screen

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from gameplay import *
 # from data.saves.save import SaveManager
 import time
 
+
+def fade_image(image: pygame.surface.Surface, screen: pygame.surface.Surface, len_dir: range):
+    for i in len_dir:
+        black = pygame.surface.Surface((1920, 1080))
+        black.fill((0, 0, 0))
+        black.set_alpha(i)
+        screen.blit(image, (0, 0))
+        screen.blit(black, (0, 0))
+        pygame.display.flip()
+
+
 def main():
     pygame.init()
     pygame.mixer.init()
@@ -13,24 +24,12 @@ def main():
     pygame.display.set_caption('Five Nights At Lone Peak High')
     pygame.display.set_icon(pygame.image.load('resources/ui/icon.png').convert())
     loading_image = pygame.image.load('resources/ui/menus/main_menu/teamLogoScreen.png').convert_alpha()
-    for i in range(255, 0, -1):
-        black = pygame.surface.Surface((1920, 1080))
-        black.fill((0, 0, 0))
-        black.set_alpha(i)
-        pygame.display.get_surface().blit(loading_image, (0, 0))
-        pygame.display.get_surface().blit(black, (0,0))
-        pygame.display.flip()
+    fade_image(loading_image, pygame.display.get_surface(), range(255, 0, -1))
     pygame.display.get_surface().blit(loading_image, (0, 0))
     pygame.display.flip()
     clock = pygame.time.Clock()
     main_menu = MainMenu()
-    for i in range(255):
-        black = pygame.surface.Surface((1920, 1080))
-        black.fill((0, 0, 0))
-        black.set_alpha(i)
-        pygame.display.get_surface().blit(loading_image, (0, 0))
-        pygame.display.get_surface().blit(black, (0,0))
-        pygame.display.flip()
+    fade_image(loading_image, pygame.display.get_surface(), range(255))
     main_menu.activate()
 
     # Window Loop
